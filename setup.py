6c8481f22ac901919d5e4d18d3c95cb4781e4eef
cs-tanmay-mandaogane <tsharad@fortinet-us.com>
2022-12-08 17:16:07 +0530

    revert changes

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     name='fortisoar-connector-engine',
     packages=find_packages(include=['connectors', 'connectors.core', 'connectors.scripts', 'integrations']),
     version=f'{release_verion}-{build_num}',
-    py_modules=['setupnovernormalize'],
     description='FortiSOAR Connector Engine Library',
     author='Fortinet',
     url='https://github.com/fortinet-fortisoar/fortisoar-connector-engine',
