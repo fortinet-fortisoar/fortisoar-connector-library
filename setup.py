a4bd08cbdd9fc7e4113ce575d1bc9664ecee25fc
cs-tanmay-mandaogane <tsharad@fortinet-us.com>
2022-12-08 13:16:26 +0530

    added variable for extendibility

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ import os
 
 build_num = os.environ.get("BUILD_NUMBER", 1)
 
-print(build_num)
+release_verion='1.0.0'
 
 setup(
     name='fortisoar-connector-engine',
     packages=find_packages(include=['connectors', 'connectors.core', 'connectors.scripts', 'integrations']),
-    version=f'1.0.0-{build_num}',
+    version=f'{release_verion}-{build_num}',
     description='FortiSOAR Connector Engine Library',
     author='Fortinet',
     url='https://github.com/fortinet-fortisoar/fortisoar-connector-engine',
