05f7f1fd36f3c4132e9c7aa06bee7e766d6f2884
cs-amol-phatangade <aphatangade@fortinet.com>
2024-04-30 15:54:45 +0530

    Adding local database path parameter to execute function script

--- connectors/core/utils.py
+++ connectors/core/utils.py
@@ -3,7 +3,7 @@
   Copyright (c) 2024 Fortinet Inc
   Copyright end """
 import ast
-import json
+import json, copy
 import requests
 import logging
 from connectors.scripts.utils import read_local_data, write_local_data
@@ -29,10 +29,11 @@ api_health_check.__str__ = lambda: 'Makes Simple Http API Call'
 
 def update_connector_config(connector_name=None, version=None, updated_config={}, configId=None, agent=None):
     try:
+        old_config = copy.deepcopy(updated_config)
         if not updated_config:
             message = "Invalid input. Provide the configuration to be updated."
             raise ValueError(message)
-        local_database_path = updated_config.get("local_database_path")
+        local_database_path = old_config.pop("local_database_path")
         if not local_database_path:
             message = "Local database file path not found. Please update your connector configuration. Once you " \
                       "update the local database file path, it will automatically be added to the configurations. "
@@ -44,7 +45,7 @@ def update_connector_config(connector_name=None, version=None, updated_config={}
                 if isinstance(v, dict):
                     for config_k, config_v in v.get("config", {}).items():
                         if config_v.get("config_id") == configId:
-                            v["config"][config_k] = updated_config
+                            v["config"][config_k] = old_config
                             found_config = True
                             break
                 if found_config:
@@ -52,7 +53,7 @@ def update_connector_config(connector_name=None, version=None, updated_config={}
         elif connector_name:
             for config_k, config_v in local_data.get(connector_name).get("config", {}).items():
                 if config_v.get("config_id"):
-                    local_data[connector_name]["config"][config_k] = updated_config
+                    local_data[connector_name]["config"][config_k] = old_config
                     break
         else:
             message = "Invalid inputs. Please provide the connector configuration id or the connector name and version for the configuration is to be updated"
--- connectors/scripts/execute_operation.py
+++ connectors/scripts/execute_operation.py
@@ -1,7 +1,6 @@
 """ Copyright start
-  Copyright (C) 2008 - 2022 Fortinet Inc.
-  All rights reserved.
-  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
+  MIT License
+  Copyright (c) 2024 Fortinet Inc
   Copyright end """
 
 import inspect
@@ -10,16 +9,18 @@ import json
 import argparse
 import os
 import importlib.util
+import copy
 from connectors.scripts.utils import is_path_exist
-from connectors.scripts.utils import read_local_data
 
 
 class ExecuteOperation:
-    def __init__(self, connector_path, connector_name, config_name, operation_name, local_data_path):
+    def __init__(self, connector_path, connector_name, config_name, operation_name, connector_data, keys_to_mask, local_data_path):
         self.connector_path = connector_path
         self.connector_name = connector_name
         self.config_name = config_name
         self.operation_name = operation_name
+        self.connector_data = connector_data
+        self.keys_to_mask = keys_to_mask
         self.local_data_path = local_data_path
 
         self.connector = self.get_connector()
@@ -27,17 +28,14 @@ class ExecuteOperation:
         self.params = None
 
         self.validate_input()
-        self.local_data = read_local_data(local_data_path)
         self.load_config_params()
 
     def validate_input(self) -> None:
         if not is_path_exist(self.connector_path):
             raise Exception(f"Connector path does not exist. Path: {self.connector_path}")
-        if not is_path_exist(self.local_data_path):
-            raise Exception(f"Local data path does not exist. Path: {self.local_data_path}")
 
     def load_config_params(self) -> None:
-        conn_data = self.local_data.get(self.connector_name, {})
+        conn_data = json.loads(self.connector_data)
 
         all_config = conn_data.get("config", {})
         self.config = all_config.get(self.config_name, {})
@@ -58,23 +56,49 @@ class ExecuteOperation:
             print(e)
         return None
 
-    def execute(self):
+    def execute(self, print_result):
         try:
+            config = self.mask_keys(copy.deepcopy(self.config))
+            params = self.mask_keys(copy.deepcopy(self.params))
             initial_message = f"Operation Name: {self.operation_name}\n" \
-                              f"Configuration: {self.config}\n" \
-                              f"Parameters: {self.params}\n"
+                              f"Configuration: {config}\n" \
+                              f"Parameters: {params}\n"
             print(initial_message)
-
-            conn_obj = self.connector()
+            info_json_data = self.read_info_json()
+            conn_obj = self.connector(info_json=info_json_data)
+            self.config.update({"local_database_path": self.local_data_path})
             if self.operation_name == "check_health":
                 result = conn_obj.check_health(self.config)
             else:
                 result = conn_obj.execute(self.config, self.operation_name, self.params)
-            print(f"Result: {result}")
+            if print_result:
+                print(f"Result: {result}")
             return result
 
         except Exception as e:
             print(e)
+
+    def read_info_json(self):
+        try:
+            info_json_file_path = self.connector_path + '/info.json'
+            with open(info_json_file_path, 'r') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            print(f"File '{self.connector_path + '/info.json'}' not found.")
+            return None
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON from file '{self.connector_path + '/info.json'}'.")
+            return None
+        finally:
+            file.close()
+
+
+    def mask_keys(self, json_data):
+        for key in json_data.keys():
+            if key in self.keys_to_mask:
+                json_data[key] = "********"
+        return json_data
+
 
 
 def main():
@@ -83,12 +107,14 @@ def main():
     parser.add_argument("--connector-name", type=str, required=True, help="Connector name")
     parser.add_argument("--config-name", type=str, required=True, help="Configuration name")
     parser.add_argument("--operation-name", type=str, required=True, help="Operation name")
+    parser.add_argument("--connector-data", type=str, required=True, help="Connector data")
+    parser.add_argument("--keys-to-mask", type=str, required=True, help="Connector's keys to mask")
     parser.add_argument("--local-data-path", type=str, required=True, help="Local data path")
 
     args = parser.parse_args()
     exec_action = ExecuteOperation(args.connector_path, args.connector_name, args.config_name,
-                                   args.operation_name, args.local_data_path)
-    exec_action.execute()
+                                   args.operation_name, args.connector_data, args.keys_to_mask.split(','), args.local_data_path)
+    exec_action.execute(True)
 
 
 if __name__ == "__main__":
